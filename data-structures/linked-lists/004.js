681f2825f78ad80f62e6c21e0aada8d4dc36e57e
Volkan Özçelik <volkan.ozcelik@gmail.com>
2011-12-20 09:54:53 +0200

    some more data structures.

---
+++ data-structures/linked-lists/004.js
@@ -0,0 +1,84 @@
+/*
+ * <!--
+ *  This program is distributed under
+ *  the terms of the MIT license.
+ *  Please see the LICENSE file for details.
+ * -->
+ */
+
+/*
+ * Write method to delete a single element from a linked list.
+ * Use the structure in 001.js as a template.
+ * Assume you are keeping a global reference to the head of the list.
+ * Make sure to update that reference as well.
+ */
+
+ /*____________________________________________________________________________*/
+
+/**
+ * @class {public} ListElement - denotes a list node.
+ * @param {Elem} next - the next node. (null if it's tail)
+ * @param {Integer} data - list data.
+ */
+function ListElement(next, data) {
+    this.next = next;
+    this.data = data;
+}
+
+/*
+ * A global reference to the list head.
+ */
+var head = null;
+
+/**
+ * @function {public static} deleteElement
+ *
+ * Removes the list item from the list.
+ *
+ * @param {ListElement} itemToDelete - the item to delet.
+ */
+function deleteElement(itemToDelete) {
+    var element = head;
+
+    if (itemToDelete === head) {
+        head = itemToDelete.next;
+        return;
+    }
+
+    while (element) {
+        if (element.next === itemToDelete) {
+            element.next = itemToDelete.next;
+            return;
+        }
+    }
+}
+
+/*____________________________________________________________________________*/
+
+var firstElement = new ListElement(null, 20);
+var secondElement = new ListElement(null, 30);
+var thirdElement = new ListElement(null, 40);
+
+firstElement.next = secondElement;
+secondElement.next = thirdElement;
+head = firstElement;
+
+console.log(head);
+deleteElement(secondElement);
+console.log(head);
+deleteElement(thirdElement);
+console.log(head);
+deleteElement(firstElement);
+console.log(head);
+
+/*____________________________________________________________________________*/
+
+/*
+Output: ($ /usr/bin/node 004.js)
+{ next: { next: { next: null, data: 40 }, data: 30 }, data: 20 }
+{ next: { next: null, data: 40 }, data: 20 }
+{ next: null, data: 20 }
+null
+*/
+
+
---
+++ data-structures/linked-lists/005.js
@@ -0,0 +1,102 @@
+/*
+ * <!--
+ *  This program is distributed under
+ *  the terms of the MIT license.
+ *  Please see the LICENSE file for details.
+ * -->
+ */
+
+/*
+ * Implement a stack in JavaScript using a linked list.
+ * Use the structure in 001.js as a template.
+ * Maintain a global reference to the top of the stack.
+ */
+
+ /*____________________________________________________________________________*/
+
+/**
+ * @class {public} ListElement - denotes a list node.
+ * @param {Elem} next - the next node. (null if it's tail)
+ * @param {Integer} data - list data.
+ */
+function ListElement(next, data) {
+    this.next = next;
+    this.data = data;
+}
+
+/*
+ * A reference to the top.
+ */
+var top = null;
+
+/**
+ * @function {public static} push
+ *
+ * Pushes data to the stack.
+ *
+ * @param {Integer} data - the data to push.
+ *
+ */
+function push(data) {
+    var element = new ListElement(null, data);
+    element.next = top;
+}
+
+/**
+ * @function {public static} pop
+ *
+ * Pops data from the stack.
+ *
+ * @return the popped data.
+ */
+function pop() {
+    var data = top.data;
+    top = top.next;
+    return data;
+}
+
+/*____________________________________________________________________________*/
+
+var firstElement = new ListElement(null, 20);
+var secondElement = new ListElement(null, 30);
+var thirdElement = new ListElement(null, 40);
+
+firstElement.next = secondElement;
+secondElement.next = thirdElement;
+
+top = firstElement;
+
+console.log(top);
+push(50);
+console.log(top);
+push(60);
+console.log(top);
+
+var data = null;
+
+while (top) {
+    data = pop();
+    console.log(data);
+    console.log(top);
+}
+
+/*____________________________________________________________________________*/
+
+/*
+Output: ($ /usr/bin/node 005.js)
+{ next: { next: { next: null, data: 40 }, data: 30 }, data: 20 }
+{ next: { next: { next: [Object], data: 30 }, data: 20 }, data: 50 }
+{ next: { next: { next: [Object], data: 20 }, data: 50 }, data: 60 }
+60
+{ next: { next: { next: [Object], data: 30 }, data: 20 }, data: 50 }
+50
+{ next: { next: { next: null, data: 40 }, data: 30 }, data: 20 }
+20
+{ next: { next: null, data: 40 }, data: 30 }
+30
+{ next: null, data: 40 }
+40
+null
+*/
+
+
