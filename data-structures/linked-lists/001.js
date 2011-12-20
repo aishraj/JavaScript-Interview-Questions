bee0f412a0c62e971afb65a168a877b9af46099d
Volkan Özçelik <volkan.ozcelik@gmail.com>
2011-12-20 08:25:22 +0200

    simple example

---
+++ data-structures/linked-lists/001.js
@@ -0,0 +1,29 @@
+/*
+ * Q: Create a singly linked-list structure in JavaScript.
+ * Assume that the list contains integer values.
+ */
+
+/*____________________________________________________________________________*/
+
+/**
+ * @class {public} ListElement - denotes a list node.
+ * @param {Elem} next - the next node. (null if it's tail)
+ * @param {Integer} data - list data.
+ */
+function ListElement(next, data) {
+	this.next = next;
+	this.data = data;
+}
+
+/*____________________________________________________________________________*/
+
+var listItem = new ListElement(null, 10);
+
+console.log(listItem);
+console.log(listItem.data);
+
+/*
+Output: ($ /usr/bin/node 001.js)
+{ next: null, data: 10 }
+10
+*/
